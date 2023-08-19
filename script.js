811f1e2d84ff90bfdc860f9e45c049dc672bef34
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-19 13:14:37 +0300

    asd

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-window.location.href = 'https://oauth.vk.com/authorize?client_id=51731162&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1/&scope=wall&response_type=token&v=5.131&state=123456'
-// window.location.href =  'https://oauth.vk.com/authorize?client_id=51729989&display=page&redirect_uri=https://lensorf.github.io/wb-L1-19&scope=wall&response_type=token&v=5.131&state=123456'
+window.location.href = 'https://oauth.vk.com/authorize?client_id=51732099&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1/&scope=wall&response_type=token&v=5.131&state=123456'
 const token = window.location.hash.split("=")[1].split("&")[0];
 
 const owner_id = -51873373;
 const version = 5.131;
 const count = 10;
-let offset = 0;
+let offset = '';
 
 const renderPosts = () => {
     VK.Api.call(
