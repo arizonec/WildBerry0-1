37bfebd578f0ec09a97d0e5971e86581355cf5e0
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-19 10:45:20 +0300

    offline

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.location.href = 'https://oauth.vk.com/authorize?client_id=51731162&redirect_uri=https://arizonec.github.io/WildBerry0-1/&display=page&scope=wall,offline&response_type=token'
+window.location.href = 'https://oauth.vk.com/authorize?client_id=51732012&redirect_uri=https://arizonec.github.io/WildBerry0-1/&display=page&scope=wall&response_type=token'
 const token = window.location.hash.split("=")[1].split("&")[0];
 
 const postLists = document.querySelector('.posts-list');
@@ -6,7 +6,7 @@ const postLists = document.querySelector('.posts-list');
 const owner_id = -51873373;
 const version = 5.131;
 const count = 10;
-let offset = 0;
+let offset = '';
 
 const renderPosts = () => {
     VK.Api.call(
