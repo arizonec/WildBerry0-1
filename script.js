4cbeb788127dea755472dbdc9930725b07f57025
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-19 15:19:17 +0300

    123

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// window.location.href = 'https://oauth.vk.com/authorize?client_id=51732152&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1/&scope=wall&response_type=token&v=5.131&state=123456'
+window.location.href = 'https://oauth.vk.com/authorize?client_id=51732152&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1/&scope=wall&response_type=token&v=5.131&state=123456'
 const token = window.location.hash.split("=")[1].split("&")[0];
 
 const owner_id = -41152133;
@@ -17,9 +17,11 @@ const renderPosts = () => {
         v: 5.131,
       },
         (data) => {
-        if (data.response) {
-            console.log(data.response);
-        }
+            if(data) {
+                console.log(true);
+            } else {
+                console.log(false);
+            }
       }
     );
   };
