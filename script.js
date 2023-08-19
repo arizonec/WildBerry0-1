a22798738b14943104015b322b0e6c871c75a7cb
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-19 12:56:03 +0300

    asd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-window.location.href = 'https://oauth.vk.com/authorize?client_id=51731162&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1&scope=wall&response_type=token&v=5.131&state=123456'
-                      //  https://oauth.vk.com/authorize?client_id=51729989&display=page&redirect_uri=https://lensorf.github.io/wb-L1-19&scope=wall&response_type=token&v=5.131&state=123456
+// window.location.href = 'https://oauth.vk.com/authorize?client_id=51731162&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1&scope=wall&response_type=token&v=5.131&state=123456'
+window.location.href =  'https://oauth.vk.com/authorize?client_id=51729989&display=page&redirect_uri=https://lensorf.github.io/wb-L1-19&scope=wall&response_type=token&v=5.131&state=123456'
 const token = window.location.hash.split("=")[1].split("&")[0];
 
 const owner_id = -51873373;
