21625aa2a31b6af307f3a4c00c95a1c86b1395bc
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-19 15:55:45 +0300

    try

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // window.location.href = 'https://oauth.vk.com/authorize?client_id=51732152&display=page&redirect_uri=https://arizonec.github.io/WildBerry0-1/&scope=wall&response_type=token&v=5.131&state=123456'
 const token = window.location.hash.split("=")[1].split("&")[0];
 
+const postsList = document.querySelector('.posts-list');
+
 const owner_id = -41152133;
 const version = 5.131;
 const count = 10;
@@ -17,10 +19,22 @@ const renderPosts = () => {
         v: 5.131,
       },
         (data) => {
-            if(data) {
-                console.log(true);
-            } else {
-                console.log(false);
+            if(data.responce) {
+              const newPosts = data.responce.items;
+              const html = newPosts //! создаём новый массив при помощи метода map который вернёт нам вёрстку новых элементов для дальнейших действий
+        .map(
+          (p) => `
+          <li class="vk-widget-post">
+            <div class="vk-widget-post-title">${p.text}</div>
+            <div class="vk-widget-post-date">${new Date(
+              p.date * 1000
+            ).toLocaleDateString()}</div>
+            <img class=ImgAll src=${p.attachments[0]['photo']?.sizes[4].url}
+          </li>
+        `
+        )
+        .join(''); //! соединяем верстку
+      postsList.insertAdjacentHTML('beforeend', html); //! добавляем посты в список
             }
       }
     );
