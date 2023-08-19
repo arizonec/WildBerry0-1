e6cefffdf097e3bb5465b19c69f3865c39518213
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-19 11:41:55 +0300

    error

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.location.href = 'https://oauth.vk.com/authorize?client_id=51732012&redirect_uri=https://arizonec.github.io/WildBerry0-1/&scope=wall&response_type=token&v=5.131&state=12345'
 const token = window.location.hash.split("=")[1].split("&")[0];
 
-const postLists = document.querySelector('.posts-list');
-
 const owner_id = -51873373;
 const version = 5.131;
 const count = 10;
@@ -18,7 +16,7 @@ const renderPosts = () => {
         access_token: token,
         v: 5.131,
       },
-       (data) => {
+        (data) => {
         if (data.response) {
             console.log(data.response);
         }
