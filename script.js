47424624e0993749c7235b748fe1315432ecc153
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-20 14:48:05 +0300

    scroller

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function loadPosts() { //! объявляем функцию загрузки п
         )
         .join(''); //соединяем верстку
       postsList.insertAdjacentHTML('beforeend', html); //добавляем посты в список
-      posts = newPosts; //добавляем посты в массив для кэширования
+      posts = [...posts, ...newPosts]; //добавляем посты в массив для кэширования
       offset += count; //увеличиваем смещение //Устанавливаем слежку за последним элементом
       observer.observe(document.querySelector('.vk-widget-post:last-child'));
       saveData(offset, posts);
