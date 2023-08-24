1464a79d65a07c72207c6e78296b1951abe5b43a
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-24 15:40:14 +0300

    last fixes

--- script.js
+++ script.js
@@ -70,10 +70,10 @@ const observer = new IntersectionObserver(callback, options);
 
 const saveData = (offset, posts) => {
     const oldPosts = JSON.parse(localStorage.getItem('posts')) ?? [];
-    const newItems = JSON.stringify([...oldPosts, ...posts]);
+    const newPosts = JSON.stringify([...oldPosts, ...posts]);
 
 
-    localStorage.setItem('posts', newItems);
+    localStorage.setItem('posts', newPosts);
     localStorage.setItem('offset', offset);
 }
 
